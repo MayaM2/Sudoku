3459f5eb14e52e5e3413d221a77a48fcff9de7d1
Maya Mann <maya.mann2@gmail.com>
2018-08-15 12:19:37 +0300

    Update .h file of Game.c

--- Game.h
+++ Game.h
@@ -8,12 +8,18 @@
 #ifndef GAME_H_
 #define GAME_H_
 int isErroneous(int col, int row);
-void printBoard();
-
-/* MAYA: */
-void doCommand(Command* command);
-int generate();
-int hint();
-int validate();
-
+int isBoardErroneous(void);
+void printBoard(void);
+int numInRange(int num, int lower, int upper);
+int validate(int** board,int blockHeight, int blockWidth, int dim, int printMessage);
+int allfilled(int** board, int dim);
+int isBoardEmpty(int** board, int dim);
+void MainMemoryFreer();
+void MainMemoryCreator();
+void OpenFileHelper(char* fileName);
+void printsForRedoUndo(int isUndo, int row, int col, int num1, int num2);
+int problemCellAssignment(int i, int j);
+int randomFill(int X,int *arri,int *arrj);
+int generate(int X, int Y, int dim);
+void doCommand(Command* inpCommand);
 #endif /* GAME_H_ */
