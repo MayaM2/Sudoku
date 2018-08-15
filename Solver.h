854d0e42b1b8bdcd419e03c01cd69a79aa3fb578
Maya Mann <maya.mann2@gmail.com>
2018-08-15 12:25:11 +0300

    Update .h file of Solver.h

--- Solver.h
+++ Solver.h
@@ -8,10 +8,12 @@
 #ifndef SOLVER_H_
 #define SOLVER_H_
 
+void updateNeighbors(int** board, int row, int col,int blockHeight, int blockWidth, int* neighborsBin);
+int isObv(int** board, int row, int col,int blockHeight, int blockWidth, int* neighborsBin);
 void Autofill(int** board, UndoRedoList *urli, int blockHeight, int blockWidth);
-
+void nextCell(int row, int col, int isForward,int dim, int* indexes);
+void demoPrint(int sols, int dim, int** board);
 int numSols(int** board, int blockHeight, int blockWidth);
-
-int ILPSolver();
+int ILPSolver(int **board,int**fixed,int**solvedBoard,int blockHeight,int blockWidth, int dim);;
 
 #endif /* SOLVER_H_ */
