d91e31a5eb75765ee60f7571bcf0c05b2cd0f259
Maya Mann <maya.mann2@gmail.com>
2018-08-17 12:47:46 +0300

    FIX : numInRange

--- Game.c
+++ Game.c
@@ -103,7 +103,7 @@ void printBoard()
  * 0 therwise.
  */
 int numInRange(int num, int lower, int upper){
-	if (num<=lower && num>=upper){
+	if (num>=lower && num<=upper){
 		return 1;
 	}
 	return 0;
